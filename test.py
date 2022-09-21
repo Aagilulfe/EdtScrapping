b6d1faac6e281d48cf84d0b25b0d72b4a8ef87c6
agilulfe <l.nicaud@eleves-alsacienne.org>
2022-09-21 12:42:25 +0200

    updates + chromedriver

--- test.py
+++ test.py
@@ -7,12 +7,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+"""""""""""Student Info"""""""""""
 
-DRIVER_PATH = '/home/luc/Documents/chromedriver'
+student_language = "Roumain"
+student_level = "L1"
+student_UE3_1 = "ECOA130d"
+student_UE3_2 = "ECOA130g"
+
+##################################
+
+DRIVER_PATH = 'chromedriver'
 
 options = Options()
-options.headless = True    # flag to decide if the page is displayed or not
+options.headless = False    # flag to decide if the page is displayed (False) or not (True)
 options.add_argument("--window-size=1920,1200")
+# options.add_argument("--window-size=970,600") #bad dimensions
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://planning.inalco.fr/public")
@@ -21,16 +30,19 @@ with open("source_code.html", "w") as file:
     file.write(driver.page_source)
 # print(driver.page_source)
 
-delay = 3 # seconds
+delay = 3 # timeout delay in seconds
+
+#WEBPAGE
 try: # wait for the page to load
     myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[1].bouton_Edit')))
     print("Page is ready!")
 except TimeoutException:
     print("Loading of page took too much time!")
 
-driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys("Roumain L1")
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(student_language + " " + student_level)
 driver.find_element(By.ID, "GInterface.Instances[1].Instances[1].bouton_Edit").send_keys(Keys.RETURN)
 
+#MAIN PLANNING
 try: # wait for the table to load
     myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[7]')))
     print("Table is ready!")
@@ -38,10 +50,24 @@ except TimeoutException:
     print("Loading of table took too much time!")
 
 edt = driver.find_element(By.ID, "GInterface.Instances[1].Instances[7]")
-print(edt)
-edt.screenshot("edt.png")
+edt.screenshot(student_language + student_level + "_edt.png")
 
-if options.headless:
+#SWITCH TO "MATIERES" SECTION
+driver.find_element(By.ID, "GInterface.Instances[0].Instances[1]_Combo2").click()
+driver.find_element(By.ID, "GInterface.Instances[1].Instances[0].bouton_Edit").click()
+driver.find_element(By.XPATH, "//div/ul/li").click()
+
+
+#FIRST UE3 PLANNING
+try: # wait for the table to load
+    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'GInterface.Instances[1].Instances[1].bouton_Edit')))
+    print("Matieres section is ready!")
+except TimeoutException:
+    print("Loading of matieres section took too much time!")
+
+
+########################
+if not options.headless:
     time.sleep(3)
 driver.quit()
 print("Page closed.")
