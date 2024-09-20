8260a156870840d7ac59593be2aaee78d5f8b528
Aagilulfe <56174345+Aagilulfe@users.noreply.github.com>
2024-09-20 19:26:59 +0200

    Testing the factorization with several promotions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from scrapper import Scrapper
+import os
 # import time
 
 """""""""""Student Info"""""""""""
 
 STUDENT_LANGUAGE = "Roumain"
 STUDENT_LEVEL = "L1"
+STUDENT_PROMOTIONS = [("Polonais", "L3"), ("Relations International (RI)", "L3")]
 STUDENT_ELECTIVE_CLASSES = ["ECOA130d", "ECOA130g"]
 
 
@@ -13,16 +15,18 @@ STUDENT_ELECTIVE_CLASSES = ["ECOA130d", "ECOA130g"]
 DRIVER_PATH = 'drivers/geckodriver'
 SCREENSHOTS_SAVE_PATH = 'screenshots/'
 
+# Folder cleaning
+for file in os.listdir(SCREENSHOTS_SAVE_PATH):
+    os.remove(SCREENSHOTS_SAVE_PATH + file)
+
 
 # Scrapper instanciation
 scrapper_bot = Scrapper(DRIVER_PATH=DRIVER_PATH, SCREENSHOTS_SAVE_PATH=SCREENSHOTS_SAVE_PATH, headless=True)
 
 scrapper_bot.webpage_loading()
 
-# scrapper_bot.goto_promotions_section()
-scrapper_bot.get_promotion_by_name(student_language=STUDENT_LANGUAGE, student_level=STUDENT_LEVEL)
+for name in STUDENT_PROMOTIONS:
+    scrapper_bot.get_promotion_by_name(student_language=name[0], student_level=name[1])
 
-# scrapper_bot.goto_matieres_section()
 for code in STUDENT_ELECTIVE_CLASSES:
-    # scrapper_bot.goto_matieres_section()
     scrapper_bot.get_matiere_by_code(class_code=code)
